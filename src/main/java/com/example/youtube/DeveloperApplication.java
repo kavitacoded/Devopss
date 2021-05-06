6871b062f2224470a47ec7afcc647319a2b58ce4
Yogesh <Yogesh@DESKTOP-HGICOLR.mshome.net>
2021-05-06 16:53:32 +0530

    edom

---
+++ src/main/java/com/example/youtube/DeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.example.youtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperApplication.class, args);
+	}
+
+}

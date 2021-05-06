5f61e764efb056b2ac485ec6f4e8f7fc4679f25c
Yogesh <Yogesh@DESKTOP-HGICOLR.mshome.net>
2021-05-06 16:57:37 +0530

    F4 changes

--- src/main/java/com/example/youtube/model/Employee.java
+++ src/main/java/com/example/youtube/model/Employee.java
@@ -4,6 +4,39 @@ public class Employee {
 
 	
 	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
 	private String Address;
 	private String State;
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", Address=" + Address + ", State=" + State + "]";
+	}
+	public Employee(int id, String address, String state) {
+		super();
+		this.id = id;
+		Address = address;
+		State = state;
+	}
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
 }
